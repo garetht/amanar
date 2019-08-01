a2d204f4f6536a0046cc90574b70b367b9aba032
Phil Tang <ptang@seatgeek.com>
2019-08-01 19:17:01 -0400

    Sometimes Sequel Pro saves IDs as floats?!

--- sequel_pro_flow.go
+++ sequel_pro_flow.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"io/ioutil"
-
 	"fmt"
+	"io/ioutil"
 	"log"
 
 	"errors"
@@ -22,27 +21,40 @@ type SequelProRootPlist struct {
 }
 
 type SequelProPlistItem struct {
-	ColorIndex                        int64  `plist:"colorIndex"`
-	Database                          string `plist:"database"`
-	Host                              string `plist:"host"`
-	Id                                int64  `plist:"id"`
-	Name                              string `plist:"name"`
-	Port                              string `plist:"port"`
-	Socket                            string `plist:"socket"`
-	SSHHost                           string `plist:"sshHost"`
-	SSHKeyLocation                    string `plist:"sshKeyLocation"`
-	SSHKeyLocationEnabled             int    `plist:"sshKeyLocationEnabled"`
-	SSHPort                           string `plist:"sshPort"`
-	SSHUser                           string `plist:"sshUser"`
-	SSLCACertFileLocation             string `plist:"sslcaCertFileLocation"`
-	SSLCACertFileLocationEnabled      int    `plist:"sslcaCertFileLocationEnabled"`
-	SSLCertificateFileLocation        string `plist:"sslCertificateFileLocation"`
-	SSLCertificateFileLocationEnabled int    `plist:"sslCertificateFileLocationEnabled"`
-	SSLKeyFileLocation                string `plist:"sslKeyFileLocation"`
-	SSLKeyFileLocationEnabled         int    `plist:"sslKeyFileLocationEnabled"`
-	Type                              int    `plist:"type"`
-	UseSSL                            int    `plist:"useSSL"`
-	User                              string `plist:"user"`
+	ColorIndex                        int64       `plist:"colorIndex"`
+	Database                          string      `plist:"database"`
+	Host                              string      `plist:"host"`
+	Id                                interface{} `plist:"id"` // Somehow, ID can be either a float or an int
+	Name                              string      `plist:"name"`
+	Port                              string      `plist:"port"`
+	Socket                            string      `plist:"socket"`
+	SSHHost                           string      `plist:"sshHost"`
+	SSHKeyLocation                    string      `plist:"sshKeyLocation"`
+	SSHKeyLocationEnabled             int         `plist:"sshKeyLocationEnabled"`
+	SSHPort                           string      `plist:"sshPort"`
+	SSHUser                           string      `plist:"sshUser"`
+	SSLCACertFileLocation             string      `plist:"sslcaCertFileLocation"`
+	SSLCACertFileLocationEnabled      int         `plist:"sslcaCertFileLocationEnabled"`
+	SSLCertificateFileLocation        string      `plist:"sslCertificateFileLocation"`
+	SSLCertificateFileLocationEnabled int         `plist:"sslCertificateFileLocationEnabled"`
+	SSLKeyFileLocation                string      `plist:"sslKeyFileLocation"`
+	SSLKeyFileLocationEnabled         int         `plist:"sslKeyFileLocationEnabled"`
+	Type                              int         `plist:"type"`
+	UseSSL                            int         `plist:"useSSL"`
+	User                              string      `plist:"user"`
+}
+
+func (item SequelProPlistItem) IntId() (int64, error) {
+	switch i := item.Id.(type) {
+	case float64:
+		return int64(i), nil
+	case uint64:
+		return int64(i), nil
+	case int64:
+		return i, nil
+	default:
+		return 0, errors.New(fmt.Sprintf("IntId: unknown value is of incompatible type: %T", item.Id))
+	}
 }
 
 func NewSequelProFlow(config *SequelProDatasource) (spf *SequelProFlow, err error) {
@@ -59,7 +71,7 @@ func NewSequelProFlow(config *SequelProDatasource) (spf *SequelProFlow, err erro
 
 	return &SequelProFlow{
 		SequelProDatasource: *config,
-		plist:                sequelPlist,
+		plist:               sequelPlist,
 	}, nil
 }
 
@@ -74,7 +86,12 @@ func (sp *SequelProFlow) findPlistItem() (plistItem *SequelProPlistItem, foundIt
 	// N.B. the iteratees of range will be copied values. This allows
 	// us to refer to the indexed children as actual pointers
 	for i := 0; i < len(children); i++ {
-		if fmt.Sprintf("%d", children[i].Id) == sp.DatabaseUUID {
+		id, err := children[i].IntId()
+		if err != nil {
+			continue
+		}
+
+		if fmt.Sprintf("%v", id) == sp.DatabaseUUID {
 			plistItem = &children[i]
 			foundItem = true
 			break
@@ -105,9 +122,15 @@ func (sp *SequelProFlow) PersistChanges() (err error) {
 		return errors.New("Could not find a matching Sequel Database for that Sequel UUID.")
 	}
 
+	var id int64
+	id, err = plistItem.IntId()
+	if err != nil {
+		return
+	}
+
 	// These two values need to be synchronized for Sequel Pro to be able to
 	// read the correct keychain value.
-	service := fmt.Sprintf("Sequel Pro : %s (%d)", plistItem.Name, plistItem.Id)
+	service := fmt.Sprintf("Sequel Pro : %s (%d)", plistItem.Name, id)
 	account := fmt.Sprintf("%s@%s/%s", plistItem.User, plistItem.Host, plistItem.Database)
 	log.Printf("[SEQUEL PRO] Persisting username %s and password %s to service %s and account %s", plistItem.User, sp.credentials.Password, service, account)
 
