cf95d7653ecbb7153d2b6201a820f4c46b15617b
Gareth Tan <garethwtan@gmail.com>
2020-01-17 23:58:09 -0500

    Generate configuration for constants.

--- amanar_yaml_configuration.go
+++ amanar_yaml_configuration.go
@@ -2,6 +2,8 @@ package amanar
 
 import "gopkg.in/yaml.v2"
 
+
+//go:generate bash -c "npx quicktype --src-lang schema amanar_config_schema.json --package amanar --top-level Amanar --lang go | sed -E -e 's/json:\"(.+)\"/json:\"\\1\" yaml:\"\\1\"/g' > amanar_configuration.go"
 func UnmarshalYamlAmanarConfiguration(data []byte) (Amanar, error) {
 	var r Amanar
 	err := yaml.Unmarshal(data, &r)
